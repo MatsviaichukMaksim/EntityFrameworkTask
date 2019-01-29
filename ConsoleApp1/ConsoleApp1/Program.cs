6fa72d8e9fa5f8d6285a8be57b22950ad1577566
Maksim Matsviaichuk <m.matsviaichuk@gmail.com>
2019-01-29 12:22:24 +0300

    CreateDbContextAndModelUsers

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1.Models;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new UserDbContext())
+            {
+                var user = new User() { FirstName = "Vasya", LastName ="Ivanov",Email="asdadsasd",Phone="asdasd" };
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/UserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=AutoDb;Trusted_Connection=True");
+        }
+    }
+}
